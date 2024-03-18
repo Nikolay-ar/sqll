39d6e0883468ed2e6720774ae739d4c2874f99f6
Nikolay <delonartt@gmail.com>
2024-03-19 02:19:26 +0500

    mess

--- gui.py
+++ gui.py
@@ -10,6 +10,16 @@ def application():
     window.setWindowTitle("Простая программа")
     window.setGeometry(300, 250, 350, 200)
 
+    mai_text = QtWidgets.QLabel(window)
+    mai_text.setText("базовая надпись")
+    mai_text.move(100, 100)
+    mai_text.adjustSize()
+
+    btn = QtWidgets.QPushButton(window)
+    btn.move(70, 150)
+    btn.setText("нажмт на меня")
+    btn.setFixedWidth(200)
+
     window.show()
     sys.exit(app.exec_())
 
